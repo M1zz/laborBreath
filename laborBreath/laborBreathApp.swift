94700c278bc94ffd45674de3ab3b39898aff1ce6
Leeo <mizzking75@gmail.com>
2024-11-13 10:48:15 +0900

    Initial Commit

---
+++ laborBreath/laborBreathApp.swift
@@ -0,0 +1,17 @@
+//
+//  laborBreathApp.swift
+//  laborBreath
+//
+//  Created by Leeo on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct laborBreathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
